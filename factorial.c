e2eba03fedcfcf028ed9d148828f950b187d0c46
Sakib Ajam <sakibajamulubbi@gmail.com>
2026-05-03 19:50:55 +0600

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+long long factorial(int n)
+{
+    if(n == 0) return 1;
+    return n * factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%lld", factorial(n));
+    return 0;
+}
